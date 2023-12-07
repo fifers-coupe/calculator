440a65de04157033671ee5a6cac075f8572b3d0d
Stefanos Papadopoulos <152976197+fifers-coupe@users.noreply.github.com>
2023-12-07 11:52:18 +0700

    Create CalculatorEngine.h

---
+++ CalculatorEngine.h
@@ -0,0 +1,10 @@
+// CalculatorEngine.h
+#pragma once
+
+class CalculatorEngine {
+public:
+    double add(double num1, double num2);
+    double subtract(double num1, double num2);
+    double multiply(double num1, double num2);
+    double divide(double num1, double num2);
+};
